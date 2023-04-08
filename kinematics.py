4131465e8ff4127066c9e261ca363b3f700b57a6
caroll675 <zhiqinchen@berkeley.edu>
2023-04-08 01:00:57 -0700

    kinematics

--- kinematics.py
+++ kinematics.py
@@ -1,18 +1,29 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
 
-# df = pd.DataFrame({'A': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-#                    'B': [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]})
+# filter outlier points
+def filter_outlier(Dataframe, window, pcutoff):
+    body_parts = ['nose', 'leftear', 'rightear', 'neck', 'leftside', 'rightside', 'tailbase']
+    # set new column names and reset index
+    new_column_names = Dataframe.iloc[:2].apply(lambda x: '_'.join(map(str, x)), axis=0).tolist()
+    Dataframe = Dataframe.set_axis(new_column_names, axis=1)
+    Dataframe = Dataframe.drop([0, 1], axis=0)
+    Dataframe = Dataframe.reset_index(drop=True).astype(float)
 
-# # Use rolling() method to roll the dataframe with a window of 90 rows
-# rolling_df = df.rolling(window=3)
-
-# rolling_df = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).rolling(window=3)
-# # Loop through the rolling dataframe to get every rolling part
-# for i, rolled_df in enumerate(rolling_df):
-#     print(f"Rolling part {i}:")
-#     print(rolled_df)
-
+    for bp in body_parts:
+        outliers = Dataframe[Dataframe[bp + "_likelihood"] < pcutoff]
+        Dataframe.loc[outliers.index, bp + "_x"] = float('nan')
+        Dataframe.loc[outliers.index, bp + "_y"] = float('nan')
+        # fill outlier points with previous value if previous value is not outlier
+        # fill outlier points with next value if next value is not outlier
+        # fill outlier points with mean value if both previous and next values are outlier
+        Dataframe[[bp + "_x"]] = Dataframe[[bp + "_x"]].fillna(method='ffill').fillna(method='bfill')
+        Dataframe[[bp + "_y"]] = Dataframe[[bp + "_y"]].fillna(method='ffill').fillna(method='bfill')
+    return Dataframe
 
 
 # angular change
@@ -23,108 +34,139 @@ def interior_angle(p0, p1, p2):
     return np.degrees(angle)
 
 def angle(Dataframe, window=90):
-    head_body_angle_right = []
-    head_body_angle_left = []
+    angle_right = []
 
     for i in range(len(Dataframe)):
         p0 = [Dataframe['rightear_x'][i], Dataframe['rightear_y'][i]]
         p1 = [Dataframe['neck_x'][i], Dataframe['neck_y'][i]]
         p2 = [Dataframe['rightside_x'][i], Dataframe['rightside_y'][i]]
-        angle_right = interior_angle(p0, p1, p2)
-        head_body_angle_right.append(angle_right)
-    head_body_angle_right = pd.Series(head_body_angle_right).rolling(window).mean().shift(-int(window/2))
-    head_body_angle_right = head_body_angle_right.fillna(method='ffill')
-    head_body_angle_right = head_body_angle_right.fillna(method='bfill')
+        angle = interior_angle(p0, p1, p2)
+        angle_right.append(angle)
+    angle_right = pd.Series(angle_right).rolling(window).mean()
+    angle_right = angle_right.fillna(method='ffill')
+
+    angle_left = []
 
     for i in range(len(Dataframe)):
         p0 = [Dataframe['leftear_x'][i], Dataframe['leftear_y'][i]]
         p1 = [Dataframe['neck_x'][i], Dataframe['neck_y'][i]]
         p2 = [Dataframe['leftside_x'][i], Dataframe['leftside_y'][i]]
-        angle_left = interior_angle(p0, p1, p2)
-        head_body_angle_left.append(angle_left)
-    head_body_angle_left = pd.Series(head_body_angle_left).rolling(window).mean().shift(-int(window/2))
-    head_body_angle_left = head_body_angle_left.fillna(method='ffill')
-    head_body_angle_left = head_body_angle_left.fillna(method='bfill')
+        angle = interior_angle(p0, p1, p2)
+        angle_left.append(angle)
+    angle_left = pd.Series(angle_left).rolling(window).mean()
+    angle_left = angle_left.fillna(method='ffill').fillna(method='bfill')
 
-    assert len(head_body_angle_right) == len(head_body_angle_left) == len(Dataframe)
+    assert len(angle_right) == len(angle_left) == len(Dataframe)
 
-    return head_body_angle_right, head_body_angle_left
+    return angle_right, angle_left
 
+
+def angle_velocity(Dataframe, window=90):
+    angle_right, angle_left = angle(Dataframe, window=window)
+    angle_right_diff = np.diff(angle_right)
+    angle_left_diff = np.diff(angle_left)
+    angle_diff = [np.linalg.norm([angle_right_diff[i], angle_left_diff[i]]) for i in range(len(angle_right_diff))]
+    angle_diff = np.array(angle_diff)
+    angle_diff = np.hstack((angle_diff[0], angle_diff))
+    velocity = pd.Series(angle_diff).rolling(window).apply(
+        lambda x: x[window-1] - x[0], raw=True)
+    velocity = velocity.fillna(method='ffill').fillna(method='bfill')
+    period = 1/30*window
+    velocity = np.abs(velocity/period)
+    assert len(velocity) == len(Dataframe)
+    return velocity
+
+
+def angle_acceleration(Dataframe, window=90):
+    velocity = angle_velocity(Dataframe, window=window)
+    angle_velocity_diff = np.diff(velocity)
+    angle_velocity_diff = np.hstack((angle_velocity_diff[0], angle_velocity_diff))
+    acceleration = pd.Series(angle_velocity_diff).rolling(window).apply(
+        lambda x: x[window-1] - x[0], raw=True)
+    acceleration = acceleration.fillna(method='ffill').fillna(method='bfill')
+    period = 1/30*window
+    acceleration = acceleration/period
+    assert len(acceleration) == len(Dataframe)
+    return acceleration
 
 # velocity
-def velocity(Dataframe, window=90, min_periods=90):
-    centroid_x = Dataframe[['nose_x', 'leftear_x', 'rightear_x', 'neck_x', 'tailbase_x']].mean(axis=1)
-    centroid_y = Dataframe[['nose_y', 'leftear_y', 'rightear_y', 'neck_y', 'tailbase_y']].mean(axis=1)
-    centroid_x_diff = np.diff(centroid_x)
-    centroid_y_diff = np.diff(centroid_y)
-    centroid = [np.linalg.norm([centroid_x_diff[i], centroid_y_diff[i]]) for i in range(len(centroid_x_diff))]
-    centroid = np.array(centroid)
-    centroid = np.hstack((centroid[0], centroid))
-    centroid_velocity = pd.Series(centroid).rolling(window, min_periods).apply(
-        lambda x: x[window-1] - x[0], raw=True).shift(-int(window/2))
-    centroid_velocity= centroid_velocity.fillna(method='ffill', limit=min_periods).fillna(method='bfill', limit=min_periods)
-    centroid_velocity = centroid_velocity/window
-    assert len(centroid_velocity) == len(Dataframe)
-    return centroid_velocity
+def velocity(Dataframe, body_parts, window=90):
+    # body_parts = ['nose', 'leftear', 'rightear', 'neck', 'tailbase']
+    body_parts = [bp + '_x' for bp in body_parts] + [bp + '_y' for bp in body_parts]
+    position_x = Dataframe[body_parts].mean(axis=1)
+    position_y = Dataframe[body_parts].mean(axis=1)
+    position_diff_x = np.diff(position_x)
+    position_diff_y = np.diff(position_y)
+    position_diff = [np.linalg.norm([position_diff_x[i], position_diff_y[i]]) for i in range(len(position_diff_x))]
+    position_diff = np.array(position_diff)
+    position_diff = np.hstack((position_diff[0], position_diff))
+    velocity = pd.Series(position_diff).rolling(window).apply(
+        lambda x: x[window-1] - x[0], raw=True)
+    velocity= velocity.fillna(method='ffill').fillna(method='bfill')
+    period = 1/30*window 
+    velocity = np.abs(velocity/period)
+    assert len(velocity) == len(Dataframe)
+    return velocity
 
 # acceleration
-def acceleration(Dataframe, window=90, min_periods=90):
-    centroid_velocity = velocity(Dataframe, window=90, min_periods=90)
-    centroid_acceleration= np.diff(centroid_velocity)
-    centroid_acceleration = np.hstack((centroid_acceleration[0], centroid_acceleration))
-    centroid_acceleration = pd.Series(centroid_acceleration).rolling(window, min_periods).apply(
-        lambda x: x[window-1] - x[0], raw=True).shift(-int(window/2))
-    centroid_acceleration = centroid_acceleration.fillna(method='ffill', limit=min_periods).fillna(method='bfill', limit=min_periods)
-    centroid_acceleration = centroid_acceleration/window
-    assert len(centroid_acceleration) == len(Dataframe)
-    return centroid_acceleration
+def acceleration(Dataframe, body_parts, window=90):
+    velocity_computed = velocity(Dataframe, body_parts, window=window)
+    acceleration= np.diff(velocity_computed)
+    acceleration = np.hstack((acceleration[0], acceleration))
+    acceleration = pd.Series(acceleration).rolling(window).apply(
+        lambda x: x[window-1] - x[0], raw=True)
+    acceleration = acceleration.fillna(method='ffill').fillna(method='bfill')
+    period = 1/30*window
+    acceleration = acceleration/period
+    assert len(acceleration) == len(Dataframe)
+    return acceleration
 
 
-def main(Dataframe, window=90, min_periods=90):
+def main(Dataframe, window=90,  body_parts = ['nose', 'leftear', 'rightear', 'neck', 'tailbase']):
+    Dataframe = filter_outlier(Dataframe, window=90, pcutoff=0.7)
     head_body_angle_right, head_body_angle_left = angle(Dataframe)
-    centroid_velocity = velocity(Dataframe, window=90, min_periods=90)
-    centroid_acceleration = acceleration(Dataframe, window=90, min_periods=90)
-    Dataframe['head_body_angle_right'] = head_body_angle_right
-    Dataframe['head_body_angle_left'] = head_body_angle_left
-    Dataframe['centroid_velocity'] = centroid_velocity
-    Dataframe['centroid_acceleration'] = centroid_acceleration
+    Dataframe['angle_right'] = head_body_angle_right
+    Dataframe['angle_left'] = head_body_angle_left
+    Dataframe['angle_velocity'] = angle_velocity(Dataframe, window=window)
+    Dataframe['angle_acceleration'] = angle_acceleration(Dataframe, window=window)
+    Dataframe['velocity'] = velocity(Dataframe, body_parts=body_parts, window=window)
+    Dataframe['acceleration'] = acceleration(Dataframe, body_parts=body_parts, window=window)
     return Dataframe
 
 
-Dataframe = pd.read_csv("test.csv")
-
-Dataframe = main(Dataframe)
-
-# save to csv
-Dataframe.to_csv("test_kinematics.csv", index=False)
 
 
+def plot(Dataframe):
 
-# ######################### UMAP ############################
-# X = main[['head_body_angle_right', 'head_body_angle_left', 
-#                  'centroid_velocity', 'centroid_acceleration']]
-# import umap.umap_ as umap
-# import matplotlib.pyplot as plt
-# reducer = umap.UMAP(n_components=3)
-# X_umap = reducer.fit_transform(X) #UMAP embedding
-# # Create a 3D scatter plot
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(X_umap[:,0], X_umap[:,1], X_umap[:, 2], alpha=0.5)
-# ax.set_xlabel('UMAP1')
-# ax.set_ylabel('UMAP2')
-# ax.set_zlabel('UMAP3')
-# plt.show()
-
-# ######################### KNN ############################
-# from sklearn.cluster import KMeans
-# kmeans = KMeans(n_clusters=3, random_state=0).fit(X_umap)
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(X_umap[:,0], X_umap[:,1], X_umap[:, 2], c=kmeans.labels_, cmap='Spectral')
-# ax.set_xlabel('UMAP1')
-# ax.set_ylabel('UMAP2')
-# ax.set_zlabel('UMAP3')
-# plt.show()
+    fig = plt.figure(figsize=(20, 10))
+    ax1 = fig.add_subplot(611)
+    ax2 = fig.add_subplot(612)
+    ax3 = fig.add_subplot(613)
+    ax4 = fig.add_subplot(614)
+    ax5 = fig.add_subplot(615)
+    ax6 = fig.add_subplot(616)
+    sns.lineplot(x=Dataframe.index, y=Dataframe['angle_right'], ax=ax1)
+    sns.lineplot(x=Dataframe.index, y=Dataframe['angle_left'], ax=ax2)
+    sns.lineplot(x=Dataframe.index, y=Dataframe['angle_velocity'], ax=ax3)
+    sns.lineplot(x=Dataframe.index, y=Dataframe['angle_acceleration'], ax=ax4)
+    sns.lineplot(x=Dataframe.index, y=Dataframe['velocity'], ax=ax5)
+    sns.lineplot(x=Dataframe.index, y=Dataframe['acceleration'], ax=ax6)
+    # align y labels position
+    fig.align_ylabels()
+    # align x axis position
+    axes = [ax1, ax2, ax3, ax4, ax5, ax6]
+    for ax in axes:
+        ax.set_xlim(0, len(Dataframe))
+    plt.show()
 
 
+############################################################################################################
+
+df_path = "C:\\Users\\chang\\DeepLabCut\\main\\JUPYTER\\DLC_Data\\videos\\2022-10-12 14-41-54DLC_resnet50_mainDec6shuffle1_120000.csv"
+body_parts = ['nose', 'leftear', 'rightear', 'neck', 'tailbase']
+
+############################################################################################################
+
+Dataframe = pd.read_csv(df_path)
+Dataframe = main(Dataframe, window=90, body_parts = body_parts)
+plot(Dataframe)
